d8057b60e5f83b9cf788ed4474e38855e618e273
vaibhavid19 <vaibhavidoiphode@gmail.com>
2018-11-19 09:55:54 -0700

    Initial Commit

---
+++ src/main/java/com/vaibhavi/thanksgivingproject/ThanksgivingprojectApplication.java
@@ -0,0 +1,12 @@
+package com.vaibhavi.thanksgivingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanksgivingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanksgivingprojectApplication.class, args);
+	}
+}
